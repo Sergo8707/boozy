e87b2139f1920606818af07da687fc5a27f98516
sergo <sergo8707@gmail.com>
2016-09-05 00:30:55 +0300

    update mail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 # Базовый класс для всех мэйлеров приложения
 class ApplicationMailer < ActionMailer::Base
   # обратный адрес всех писем по умолчанию
-  default from: "megabbq@goodprogrammer.ru"
+  default from: "boozy"
 
   # Задаем макет для всех писем
   layout 'mailer'
